ca1538b6a29782c4060261b924700dba09c5762f
Angela Ambroz <angela.ambroz@gmail.com>
2015-09-17 16:35:10 -0400

    first commit

---
+++ cleaning.py
@@ -0,0 +1,49 @@
+#!/usr/bin/local/python
+
+import csv
+import os
+import sys
+from dbfpy import dbf
+
+
+INPUT = "/Users/angelaambroz/Dropbox (Personal)/EPoD (Personal)/Data analytics/Pakistan - Commodity pricing/example-data/Price data from year 2003 to date/"
+OUTPUT = "/Users/angelaambroz/Dropbox (Personal)/EPoD (Personal)/Data analytics/Pakistan - Commodity pricing/example-data/price-data-clean/"
+DIR = "/Users/angelaambroz/Dropbox (Personal)/EPoD (Personal)/Data analytics/Pakistan - Commodity pricing/example-data/"
+BASE_YEAR = 2003
+
+
+def DBFtoCSV(filename):
+	if filename.endswith('.DBF'):
+	    
+	    print "Converting %s to csv" % filename
+	    csv_fn = filename[:-4]+ ".csv"
+	    
+	    with open(csv_fn,'wb') as csvfilename:
+	        
+	        in_db = dbf.Dbf(filename)
+	        out_csv = csv.writer(csvfilename)
+	        names = []
+	        
+	        for field in in_db.header.fields:
+	            names.append(field.name)
+	        
+	        out_csv.writerow(names)
+	        
+	        for rec in in_db:
+	            out_csv.writerow(rec.fieldData)
+	        
+	        in_db.close()
+	        
+	        print "Done..."
+	
+	else:
+	  print "Filename does not end with .DBF"
+
+
+for folder in os.listdir(INPUT):
+	if folder!=".DS_Store" and folder[-4:]!="xlsx":
+		for item in os.listdir(INPUT + folder):
+			DBFtoCSV(INPUT + folder + "/" + item)
+
+
+
